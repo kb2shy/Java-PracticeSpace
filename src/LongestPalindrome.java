fb5984a604c8e9e85de31d7a367ac7e3583f52a0
Chhaian Pin <chhaianpin@hotmail.com>
2019-12-26 13:25:00 -0800

    completed String to Integer on leetcode.com challenge #8

--- src/LongestPalindrome.java
+++ src/LongestPalindrome.java
@@ -1,7 +1,3 @@
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
-
 /**
  * LongestPalindrome
  */
@@ -11,19 +7,15 @@ public class LongestPalindrome {
     private static int maxLen;
 
     public static void main(String[] args) {
-        // System.out.println(longestPalindrome("babaddtattarrattatddetartrateedredividerb"));
-        // System.out.println(longestPalindrome("a"));
-        // System.out.println(longestPalindrome("abad"));
-        // System.out.println(longestPalindrome("cabbd"));
-        // System.out.println(longestPalindrome("dap"));
+        System.out.println(longestPalindrome("babaddtattarrattatddetartrateedredividerb"));
+        System.out.println(longestPalindrome("a"));
+        System.out.println(longestPalindrome("abad"));
+        System.out.println(longestPalindrome("cabbd"));
+        System.out.println(longestPalindrome("dap"));
         System.out.println(longestPalindrome("asecadanadn"));
     }
 
     public static String longestPalindrome(String s) {
-
-        /*
-        Solution from discussion
-        */
 
         int len = s.length();
         if (len < 2) {
@@ -31,93 +23,21 @@ public class LongestPalindrome {
         }
 
         for (int i = 0; i < len - 1; i++) {
-            extendPalindrome(s, i, i);
-            extendPalindrome(s, i, i + 1);
+            makePalindrome(s, i, i);
+            makePalindrome(s, i, i + 1);
         }
 
         return s.substring(lo, lo + maxLen);
-
-        // if (s.length() < 1) {
-        //     return "";
-        // }
-
-        // Set<String> palindromes = new HashSet<>(); 
-        // String lp = "";
-        // String test = "";
-        // for (int i = 0; i < s.length(); i++) {
-        //     test = s.substring(0, i + 1);
-        //     if (isPalindrome(test) && (test.length() > lp.length())) {
-        //         lp = test;
-        //         palindromes.add(test);
-        //         System.out.println(test);
-        //     }
-        // }
-
-        // for (int i = 1; i < s.length(); i++) {
-        //     test = s.substring(i, s.length());
-        //     if (isPalindrome(test)&& (test.length() > lp.length())) {
-        //         lp = test;
-        //         palindromes.add(test);
-        //         System.out.println(test);
-        //     }
-        // }
-
-        // for (int i = s.length() - 1; i > s.length() / 2; i--) {
-        //     for (int j = 0; j < s.length() / 2; j++) {
-        //         test = s.substring(j, i);
-        //         if (isPalindrome(test)&& (test.length() > lp.length())) {
-        //             lp = test;
-        //             palindromes.add(test);
-        //         }
-        //     }
-        // }
-
-        // for (int i = 0; i < s.length() / 2; i++) {
-        //     for (int j = s.length(); j > s.length() / 2; j--) {
-        //         test = s.substring(i, j);
-        //         if (isPalindrome(test)&& (test.length() > lp.length())) {
-        //             lp = test;
-        //             palindromes.add(test);
-        //         }
-        //     }
-        // }
-
-        // String lp = "";
-        // for (String string : palindromes) {
-        //     if (string.length() > lp.length()) {
-        //         lp = string;
-        //     }
-        // }
-
-        // return lp;
     }
 
-    // public static boolean isPalindrome(String s) {
-    //     char[] chars = s.toCharArray();
-    //     Stack<Character> characters = new Stack<>();
-    //     for (char x : chars) {
-    //         characters.add(x);
-    //     }
-
-    //     for (int i = 0; i < chars.length; i++) {
-    //         if (characters.peek() == chars[i]) {
-    //             characters.pop();
-    //         } else {
-    //             return false;
-    //         }
-    //     }
-        
-    //     return characters.isEmpty() || characters.size() == 1;
-    // }
-
-    public static void extendPalindrome(String s, int j, int k) {
-        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
-            j--;
-            k++;
+    public static void makePalindrome(String s, int leftIndex, int rightIndex) {
+        while (leftIndex >= 0 && rightIndex < s.length() && s.charAt(leftIndex) == s.charAt(rightIndex)) {
+            leftIndex--;
+            rightIndex++;
         }
-        if (maxLen < k - j - 1) {
-            lo = j + 1;
-            maxLen = k - j - 1;
+        if (maxLen < rightIndex - leftIndex - 1) {
+            lo = leftIndex + 1;
+            maxLen = rightIndex - leftIndex - 1;
         }
     }
 }
---
+++ src/StringToInteger.java
@@ -0,0 +1,61 @@
+import java.util.regex.Pattern;
+
+class StringToInteger {
+    public static void main(String[] args) {
+        System.out.println(myAtoi("5564"));
+        System.out.println(myAtoi("     -92"));
+        System.out.println(myAtoi("words and 583"));
+        System.out.println(myAtoi("+"));
+        System.out.println(myAtoi("+2-2"));
+        System.out.println(myAtoi("+22-33"));
+        System.out.println(myAtoi("     -1 123"));
+        System.out.println(myAtoi("20000000000000000000"));
+        System.out.println(myAtoi("5-1"));
+    }
+
+    public static int myAtoi(String str) {
+        String regex = "\\d";
+        char[] strArray = str.toCharArray();
+        String newNumberString = "";
+        for (int i = 0; i < strArray.length; i++) {
+            if (Pattern.matches(regex, Character.toString(strArray[i])) || 
+                strArray[i] == '+' || 
+                strArray[i] == '-') {
+                if ((strArray[i] == '-' || 
+                     strArray[i] == '+') && 
+                     (newNumberString.startsWith("-") || 
+                      newNumberString.startsWith("+") ||
+                      newNumberString.length() > 0)) {
+                    break;
+                } else {
+                    newNumberString += strArray[i];
+                } 
+            } else if (strArray[i] == ' ' && newNumberString.length() == 0) {
+                continue;
+            } else {
+                break;
+            }
+        }
+
+        return newNumberString.length() != 0 ? checkInt(newNumberString) : 0;
+    }
+
+    private static int checkInt(String numStr) {
+        if (numStr.equals("+") || numStr.equals("-")) {
+            return 0;
+        // } else if ((numStr.length() > 11 && numStr.startsWith("-")) || Long.parseLong(numStr) < Integer.MIN_VALUE) {
+        //     return Integer.MIN_VALUE;
+        // } else if (numStr.length() > 10 && Long.parseLong(numStr) > Integer.MAX_VALUE) {
+        //     return Integer.MAX_VALUE;
+        }
+        try {
+            return Integer.parseInt(numStr);
+        } catch (NumberFormatException e) {
+            if (numStr.startsWith("-")) {
+                return Integer.MIN_VALUE;
+            } else {
+                return Integer.MAX_VALUE;
+            }
+        }
+    }
+}
